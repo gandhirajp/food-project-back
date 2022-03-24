8dc8a04ac2eac9b81ed0c95c077c3ad5bd1bb684
gandhirajp <gandhiraj007p@gmail.com>
2022-03-24 18:01:50 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require("express");
+const Pizza=require("./models/pizzaModel")
+const db=require("./db.js")
+const app=express();
+
+app.use(express.json());
+ 
+const pizzasRoute =require("./routes/pizzasRoute")
+const userRoute =require("./routes/userRoute")
+const ordersRoute =require("./routes/ordersRoute")
+
+
+app.use('/api/pizzas/',pizzasRoute)
+app.use('/api/users/',userRoute)
+// order delete
+ app.use('/api/orders/',ordersRoute)
+// order delete
+app.get("/",(req,res)=>{
+    res.send("Server Working") 
+})
+
+
+ 
+app.listen(process.env.PORT || 3001)  
